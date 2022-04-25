4a8cbaa5c8f968f9bc1820f4d271b557c0c8c9b1
RABEMAMPIANDRA Eric <memsjava@gmail.com>
2022-04-25 20:43:24 +0300

    andrana 1

--- core/wallet/crypto.py
+++ core/wallet/crypto.py
@@ -3,3 +3,18 @@
 # WALLET FOR CRYPTO
 ############################ 
 """
+from bitcoinlib.wallets import HDWallet, wallet_delete
+from bitcoinlib.mnemonic import Mnemonic
+
+
+class crypto():
+    def __init__(self):
+        self.passphrase = Mnemonic().generate()
+        print(self.passphrase)
+        pass
+
+    def create_btc(self):
+        wallet = HDWallet.create(
+            "mWallet1", keys=self.passphrase, network='bitcoin')
+        key1 = wallet.new_key()
+        print(key1.address)
